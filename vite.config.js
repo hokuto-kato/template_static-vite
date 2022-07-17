fca7bc5af77a13748ac6e0e7130d0d06b1a2d3b2
hokuto-kato <hokuto15@gmail.com>
2022-07-18 08:07:04 +0900

    pug,squoosh,asset設定

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,45 @@
-import {defineConfig} from "vite";
-import viteImagemin from "vite-plugin-imagemin";
+import { defineConfig } from "vite"
+import input from "@macropygia/vite-plugin-glob-input"
+// import imagemin from "@macropygia/vite-plugin-imagemin-cache"
+import pug from "@macropygia/vite-plugin-pug-static"
+import squoosh from "./plugin/squoosh.js"
 
-export default defineConfig({
-	base: "./",
-	root: "src",
-	build: {
-		outDir: "../dist",
-		emptyOutDir: true
-	},
-	plugins: [
-		viteImagemin({
-		// 	gifsicle: {
-		// 		optimizationLevel: 7,
-		// 		interlaced: false,
-		// 	},
-		// 	optipng: {
-		// 		optimizationLevel: 7,
-		// 	},
-		// 	mozjpeg: {
-		// 		quality: 20,
-		// 	},
-		// 	pngquant: {
-		// 		quality: [0.8, 0.9],
-		// 		speed: 4,
-		// 	},
-		// 	svgo: {
-		// 		plugins: [
-		// 			{
-		// 				name: "removeViewBox",
-		// 			},
-		// 			{
-		// 				name: "removeEmptyAttrs",
-		// 				active: false,
-		// 			},
-		// 		],
-		// 	},
-		}),
-	],
-});
+export default defineConfig(({ mode }) => {
+	console.log("** mode **", mode)
+	return {
+		base: "./",
+		root: "src",
+		server: {
+			open: true,
+			host: true,
+		},
+		build: {
+			outDir: "../dist",
+			emptyOutDir: true,
+			sourcemap: mode === "develop",
+			minify: mode === "production" ? "terser" : false,
+			rollupOptions: {
+				output: {
+					chunkFileNames: "js/app.[hash].js",
+					entryFileNames: "js/app.[hash].js",
+					assetFileNames: ({ name }) => {
+						if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+							return "img/[name].[hash][extname]"
+						}
+						if (/\.css$/.test(name ?? "")) {
+							return "css/app.[hash][extname]"
+						}
+						return "assets/[name].[hash][extname]"
+					},
+				},
+			},
+		},
+		plugins: [
+			pug(),
+			squoosh(),
+			input({
+				patterns: ["src/**/[^_]*.pug"],
+			}),
+		],
+	}
+})
