74986e12e5295d6fc19d8c40cae970e95d37bc07
hokuto-kato <hokuto15@gmail.com>
2022-08-16 22:29:04 +0900

    js to ts. js standard style.

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite'
+import input from '@macropygia/vite-plugin-glob-input'
+import pug from '@macropygia/vite-plugin-pug-static'
+import squoosh from './plugin/squoosh'
+
+export default defineConfig(({ mode }) => {
+	console.log('** mode **', mode)
+	return {
+		base: './',
+		root: 'src',
+		server: {
+			open: true,
+			host: true
+		},
+		css: {
+			devSourcemap: true
+		},
+		build: {
+			outDir: '../dist',
+			emptyOutDir: true,
+			sourcemap: mode === 'develop',
+			minify: mode === 'production' ? 'terser' : false,
+			rollupOptions: {
+				output: {
+					chunkFileNames: 'js/app.[hash].js',
+					entryFileNames: 'js/app.[hash].js',
+					assetFileNames: ({ name }) => {
+						if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+							return 'img/[name].[hash][extname]'
+						}
+						if (/\.css$/.test(name ?? '')) {
+							return 'css/app.[hash][extname]'
+						}
+						return 'assets/[name].[hash][extname]'
+					}
+				}
+			}
+		},
+		plugins: [
+			pug(),
+			squoosh(),
+			input({
+				patterns: ['src/**/[^_]*.pug']
+			})
+		]
+	}
+})
