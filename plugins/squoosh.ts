b1fa39ed6c599476a1af210ea157e3d755a345b3
hokuto-kato <hokuto15@gmail.com>
2022-08-23 01:00:09 +0900

    rename

---
+++ plugins/squoosh.ts
@@ -0,0 +1,61 @@
+import { ImagePool } from '@squoosh/lib'
+import { cpus } from 'os'
+
+global.navigator = undefined
+const imagePool = new ImagePool(cpus().length)
+
+// JPGの圧縮オプション
+const jpgEncodeOptions = {
+	mozjpeg: { quality: 75 }
+}
+
+// PNGの圧縮オプション
+const pngEncodeOptions = {
+	oxipng: {
+		effort: 2
+	}
+}
+
+export default function squoosh () {
+	return {
+		name: 'squoosh',
+		async generateBundle (_options, bundle) {
+			// JPGとPNGを抽出
+			const imageFileList = Object.keys(bundle).filter((key) => {
+				const regex = /\.(jpe?g|png)$/i
+				return regex.test(key)
+			})
+			const imagePoolList = imageFileList.map((file) => {
+				const imageSource = bundle[file].source
+				const image = imagePool.ingestImage(imageSource)
+				return { file, image }
+			})
+
+			await Promise.all(
+				imagePoolList.map(async (item) => {
+					const { image, file } = item
+					if (/\.(jpe?g)$/i.test(file)) {
+						await image.encode(jpgEncodeOptions)
+					}
+					if (/\.(png)$/i.test(file)) {
+						await image.encode(pngEncodeOptions)
+					}
+				})
+			)
+
+			for (const item of imagePoolList) {
+				const { image, file } = item
+				let data
+				if (/\.(jpe?g)$/i.test(file)) {
+					data = await image.encodedWith.mozjpeg
+				}
+				if (/\.(png)$/i.test(file)) {
+					data = await image.encodedWith.oxipng
+				}
+				bundle[file].source = data.binary
+			}
+
+			await imagePool.close()
+		}
+	}
+}
